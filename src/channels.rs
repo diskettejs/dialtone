08b4cf68423413b87e71af3ca031bcdd96ac8ca7
Daniel Sandiego <dnlsandiego@gmail.com>
2026-07-02 22:42:21 -0500

    refactor/simplify handlers

--- src/channels.rs
+++ src/channels.rs
@@ -2,12 +2,10 @@ use napi_derive::napi;
 use zenoh::handlers as zhandlers;
 use zenoh::handlers::IntoHandler;
 
-use crate::handlers::IntoZenoh;
-
 #[derive(Clone, Default)]
 #[napi]
 pub struct FifoChannel {
-  capacity: Option<usize>,
+  capacity: usize,
 }
 
 #[napi]
@@ -15,18 +13,7 @@ impl FifoChannel {
   #[napi(constructor)]
   pub fn new(capacity: u32) -> Self {
     Self {
-      capacity: Some(capacity as usize),
-    }
-  }
-}
-
-impl IntoZenoh for FifoChannel {
-  type Into = zhandlers::FifoChannel;
-
-  fn into_zenoh(self) -> Self::Into {
-    match self.capacity {
-      Some(capacity) => zhandlers::FifoChannel::new(capacity),
-      None => zhandlers::FifoChannel::default(),
+      capacity: capacity as usize,
     }
   }
 }
@@ -35,7 +22,7 @@ impl<T: Send + 'static> IntoHandler<T> for FifoChannel {
   type Handler = zhandlers::FifoChannelHandler<T>;
 
   fn into_handler(self) -> (zhandlers::Callback<T>, Self::Handler) {
-    self.into_zenoh().into_handler()
+    zhandlers::FifoChannel::new(self.capacity).into_handler()
   }
 }
 
@@ -55,18 +42,10 @@ impl RingChannel {
   }
 }
 
-impl IntoZenoh for RingChannel {
-  type Into = zhandlers::RingChannel;
-
-  fn into_zenoh(self) -> Self::Into {
-    zhandlers::RingChannel::new(self.capacity)
-  }
-}
-
 impl<T: Send + 'static> IntoHandler<T> for RingChannel {
   type Handler = zhandlers::RingChannelHandler<T>;
 
   fn into_handler(self) -> (zhandlers::Callback<T>, Self::Handler) {
-    self.into_zenoh().into_handler()
+    zhandlers::RingChannel::new(self.capacity).into_handler()
   }
 }
--- src/handlers.rs
+++ src/handlers.rs
@@ -1,33 +1,24 @@
 use std::future::Future;
-use std::marker::PhantomData;
 use std::pin::Pin;
 
+use async_trait::async_trait;
 use napi::Unknown;
 use napi::bindgen_prelude::*;
 use zenoh::handlers::{self as zhandlers, IntoHandler};
 
-use crate::channels::{FifoChannel, RingChannel};
-use crate::error::MapNapiErr;
+use crate::{channels::*, error::MapNapiErr};
 
-type RustCallback<T> = zhandlers::Callback<T>;
 type RecvFuture<'a, T> = Pin<Box<dyn Future<Output = napi::Result<T>> + Send + 'a>>;
 
-pub(crate) enum HandlerImpl<T> {
-  Rust(Box<dyn Receiver<T> + Send + Sync>),
-  // JavaScript(CustomObject)
-}
+pub(crate) struct HandlerImpl<T>(Box<dyn Receiver<T>>);
 
 impl<T> HandlerImpl<T> {
   pub(crate) fn recv(&self) -> RecvFuture<'_, T> {
-    match self {
-      Self::Rust(receiver) => receiver.recv(),
-    }
+    self.0.recv()
   }
 
   pub(crate) fn try_recv(&self) -> napi::Result<Option<T>> {
-    match self {
-      Self::Rust(receiver) => receiver.try_recv(),
-    }
+    self.0.try_recv()
   }
 }
 
@@ -37,69 +28,55 @@ pub(crate) trait IntoZenoh: 'static {
   fn into_zenoh(self) -> Self::Into;
 }
 
+#[async_trait]
 pub(crate) trait Receiver<T>: Send + Sync {
-  fn recv(&self) -> RecvFuture<'_, T>;
+  async fn recv(&self) -> napi::Result<T>;
   fn try_recv(&self) -> napi::Result<Option<T>>;
 }
 
-struct RustHandler<H, T>
-where
-  H: IntoZenoh,
-  H::Into: IntoHandler<T>,
-{
-  handler: <H::Into as IntoHandler<T>>::Handler,
-  _phantom: PhantomData<T>,
-}
-
 macro_rules! impl_receiver {
-  ($($channel:ident),* $(,)?) => {$(
-    impl<T: Send + Sync + 'static> Receiver<T> for RustHandler<$channel, T> {
-      fn recv(&self) -> RecvFuture<'_, T> {
-        Box::pin(async move { self.handler.recv_async().await.map_napi_err() })
+  ($($handler:ident),* $(,)?) => {$(
+    #[async_trait]
+    impl<T: Send + 'static> Receiver<T> for zhandlers::$handler<T> {
+      async fn recv(&self) -> napi::Result<T> {
+        self.recv_async().await.map_napi_err()
       }
 
       fn try_recv(&self) -> napi::Result<Option<T>> {
-        self.handler.try_recv().map_napi_err()
+        Self::try_recv(self).map_napi_err()
       }
     }
   )*};
 }
-impl_receiver!(FifoChannel, RingChannel);
+impl_receiver!(FifoChannelHandler, RingChannelHandler);
 
-fn rust_handler<H, T>(channel: H) -> (RustCallback<T>, HandlerImpl<T>)
+fn erased<C, T>(channel: C) -> (zhandlers::Callback<T>, HandlerImpl<T>)
 where
-  H: IntoZenoh,
-  H::Into: IntoHandler<T>,
-  <H::Into as IntoHandler<T>>::Handler: Send + Sync + 'static,
-  RustHandler<H, T>: Receiver<T>,
-  T: Send + Sync + 'static,
+  C: IntoHandler<T>,
+  C::Handler: Receiver<T> + 'static,
 {
-  let (callback, handler) = channel.into_zenoh().into_handler();
-  let rust_handler = RustHandler::<H, T> {
-    handler,
-    _phantom: PhantomData,
-  };
-  (callback, HandlerImpl::Rust(Box::new(rust_handler)))
+  let (callback, handler) = channel.into_handler();
+  (callback, HandlerImpl(Box::new(handler)))
 }
 
 pub(crate) fn into_handler<'a, T>(
   handler: Option<Unknown<'a>>,
 ) -> napi::Result<impl IntoHandler<T, Handler = HandlerImpl<T>> + use<T>>
 where
-  T: Send + Sync + 'static,
+  T: Send + 'static,
 {
   let Some(obj) = handler else {
-    return Ok(rust_handler::<FifoChannel, T>(FifoChannel::default()));
+    return Ok(erased(zhandlers::FifoChannel::default()));
   };
 
-  let channel = Either::<
-    ClassInstance<'_, FifoChannel>,
-    ClassInstance<'_, RingChannel>,
-  >::from_unknown(obj)
-  .map_err(|_| napi::Error::from_reason("Invalid handler type"))?;
+  let channel =
+    Either::<ClassInstance<'_, FifoChannel>, ClassInstance<'_, RingChannel>>::from_unknown(obj)
+      .map_err(|_| {
+        napi::Error::from_reason("Invalid handler type: expected FifoChannel or RingChannel")
+      })?;
 
   Ok(match channel {
-    Either::A(h) => rust_handler::<FifoChannel, T>((*h).clone()),
-    Either::B(h) => rust_handler::<RingChannel, T>((*h).clone()),
+    Either::A(h) => erased((*h).clone()),
+    Either::B(h) => erased((*h).clone()),
   })
 }
